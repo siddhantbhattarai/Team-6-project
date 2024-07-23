5297c79d24a6824b2ad0205f60ce027f922d3e1f
Raj Kumar Nepal <rajkumarnepal8@gmail.com>
2024-07-23 15:54:55 +0545

    Added views, urls and html pages for portfolio.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('about', views.about, name='about'),
+    path('careers', views.careers, name='careers'),
+    path('contact', views.contact, name='contact'),
+    path('privacy-policy', views.about, name='privacy-policy'),
+    path('terms-of-service', views.termsofservice, name='termsofservice'),
+    path('teams', views.about, name='teams'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
+
+def privacy(request):
+    return render(request, 'portfolio/privacy-policy.html')
+
+def termsofservice(request):
+    return render(request, 'portfolio/tos.html')
+
+def teams(request):
+    return render(request, 'portfolio/teams.html')
+
+def careers(request):
+    return render(request, 'portfolio/careers.html')
